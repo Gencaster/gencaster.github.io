875859de6e612e8c60545acee8d729782bc7ec73
Philipp <p6gb@posteo.net>
2023-02-27 16:37:53 +0100

    Section Nav Component

---
+++ components/SectionNav.vue
@@ -0,0 +1,43 @@
+<script setup>
+    const sections = ["General", "Features", "Editor", "News", "Support"]
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="section in sections">
+                <a href="${section}">{{ section }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    @include padding-v(1.5em);
+}
+
+ul {
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    list-style-type: none;
+    justify-content: center;
+}
+
+li {
+    @include margin-h(1ch);
+
+    text-decoration: none;
+}
+
+a {
+    text-decoration: none;
+    color: gray;
+    transition: color 200ms ease-in-out;
+
+    &:hover {
+        color: black;
+    }
+}
+</style>
