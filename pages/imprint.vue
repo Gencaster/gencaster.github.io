d19c8f228bb27841621fbed7ddf77d88cd435317
Philipp <p6gb@posteo.net>
2023-03-01 17:27:48 +0100

    Imprint Page

---
+++ pages/imprint.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { data: imprint } = await useAsyncData("imprint", () => {
+    return queryContent("/").where({ page: 'imprint' }).findOne();
+});
+</script>
+
+<template>
+    <main>
+        <ContentRenderer :value="imprint" tag="section">
+             <ContentRendererMarkdown :value="imprint" />
+        </ContentRenderer>
+    </main>
+</template>
