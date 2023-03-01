175214b5a20e4def59d8941baf0dde184679e018
Philipp <p6gb@posteo.net>
2023-03-01 17:46:18 +0100

    Footer

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
     <Header/>
     <NuxtPage/>
+    <Footer/>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer>
+        <p>2022 – 2023</p>
+        <a href="/imprint">Imprint</a>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    footer {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-top: 5rem;
+        margin-bottom: 2rem;
+
+        a,
+        p {
+            @include padding-h(1ch);
+        }
+
+        p {
+            margin-bottom: 0;
+        }
+    }
+</style>
