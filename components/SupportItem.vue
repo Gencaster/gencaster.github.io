8557a65769b76926d6a272de1224851a165b44f7
Philipp <p6gb@posteo.net>
2023-03-13 11:26:58 +0100

    Support Tile Mobile Styling

--- components/SupportItem.vue
+++ components/SupportItem.vue
@@ -17,14 +17,15 @@ const props = defineProps({
 
 <style lang="scss" scoped>
 .support-tile {
-    @include padding-v(1em);
+    @include padding-v(1.5em);
     @include borderless;
+    @include gap;
 
     display: flex;
+    align-items: flex-start;
     flex-flow: column nowrap;
-    gap: $spacingS;
     background-color: $grey;
-
+    
     &:nth-of-type(2n) {
         background-color: $white;
     }
@@ -32,16 +33,38 @@ const props = defineProps({
     @include onScreen('tablet-portrait-up') {
         flex-direction: row;
         align-items: center;
-        gap: $spacingM;
     }
-           
+     
+    @include onScreen('tablet-landscape-up') {
+        gap: $spacingL;
+    }
 }
 
 .img-container {
-    width: 16.666667%;
+    height: 4rem;
+    width: auto;
+    margin-bottom: 1rem;
+    margin-top: 0.5rem;
+    
+    @include onScreen('tablet-portrait-up') {
+        @include margin-v(0);
+        
+        width: 16.666667%;
+        padding-right: 1rem;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
 }
 
 .text-container {
-    width: 83.333333%;
+    width: 100%;
+    
+    @include onScreen('tablet-portrait-up') {
+        width: 83.333333%;
+    }
 }
 </style>
