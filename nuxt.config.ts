efcb0860c1984555a53582df72e3d188614ef682
Philipp <p6gb@posteo.net>
2023-03-14 14:23:58 +0100

    Change Asset Path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
   ],
 
 	modules: ["@nuxt/content"],
-  css: ['assets/scss/main.scss'],
+  css: ['~/assets/scss/main.scss'],
   vite: {
     css: {
       preprocessorOptions: {
